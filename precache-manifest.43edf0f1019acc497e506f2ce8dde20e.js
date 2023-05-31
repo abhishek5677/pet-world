25f08c8f7b8ce002fd793a2b3d00e37d3883b5b5
abhishekg5677 <abhishek.g@xircls.com>
2023-05-31 22:37:23 +0530

    Updates

---
+++ precache-manifest.43edf0f1019acc497e506f2ce8dde20e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6758bade846b4361ec4",
+    "url": "/static/css/main.bce2f410.chunk.css"
+  },
+  {
+    "revision": "a6758bade846b4361ec4",
+    "url": "/static/js/main.a6758bad.chunk.js"
+  },
+  {
+    "revision": "1e49b63da51817aae6f6",
+    "url": "/static/css/1.88022c2e.chunk.css"
+  },
+  {
+    "revision": "1e49b63da51817aae6f6",
+    "url": "/static/js/1.1e49b63d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "db58cfbabdc0cef418983b1a50cc0383",
+    "url": "/index.html"
+  }
+];
